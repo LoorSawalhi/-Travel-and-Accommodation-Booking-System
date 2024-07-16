794908e47269c65065916de7c79710bb2331249d
Loor Sawalhi <loorwael@yahoo.com>
2024-07-16 10:09:45 +0300

    TABS-6 Moving search on hotels to hotel controller

---
+++ TravelAndAccommodationBookingPlatform.API/Controllers/HotelsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sieve.Models;
+using TravelAndAccommodationBookingPlatform.API.DTOs;
+using TravelAndAccommodationBookingPlatform.API.Mappers;
+using TravelAndAccommodationBookingPlatform.Domain.IServices;
+
+namespace TravelAndAccommodationBookingPlatform.API.Controllers;
+
+/// <summary>
+/// Search controller over hotels,
+/// will return hotels with matched parameters
+/// </summary>
+[ApiController]
+[Route("/api/{version:apiVersion}/hotels")]
+[ApiVersion("1")]
+public class HotelsController(IHotelService hotelService, HotelMapper hotelMapper) : ControllerBase
+{
+    /// <summary>
+    /// Search on hotels
+    /// </summary>
+    /// <response code="200">Returns matched hotels based on query</response>
+    //
+    [HttpGet("search")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<HotelDto>> SearchHotel([FromQuery] SieveModel sieveModel)
+    {
+        var models = await hotelService.FindHotelsAsync(sieveModel);
+        var hotels = models.Select(hotelMapper.MapFromDomainToAPI);
+        return Ok(hotels);
+    }
+}
